434b5903aa9ed63edeb84017591c3d22f3aa88ad
Anupam2570632 <128147498+Anupam2570632@users.noreply.github.com>
2024-05-11 14:18:40 +0600

    Food get by id method created

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oeipnk8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -31,6 +31,13 @@ async function run() {
         app.get('/foods', async (req, res) => {
             const result = await foodCollection.find().toArray()
             res.send(result);
+        })
+
+        app.get('/food/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await foodCollection.findOne(query)
+            res.send(result)
         })
 
         app.post('/foods', async (req, res) => {
